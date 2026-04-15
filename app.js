ce289239d440ff6eb5d68f98c87d0388f6c3dd04
vinukumbhar <vinayak.intcon@gmail.com>
2026-04-15 14:52:43 +0530

    mongoose error solved

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ const messageRoutes = require('./routes/messageRoutes');
 
 
 const app = express();
-app.use(cors())
+// app.use(cors())
+app.use(
+  cors({
+    origin: "https://chatleaf-messagingapp.netlify.app",
+    credentials: true,
+  })
+);
 // app.use(express.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
